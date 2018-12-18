d044a5b6880f0e4900f47434d9fe1f2d6b53241d
RPraanesh <45656825+RPraanesh@users.noreply.github.com>
2018-12-18 17:17:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/centos/.local/bin:/home/centos/bin
 echo 'pwd'
-cobc -x Multiply.cbl>IF-Example.cbl>HelloWorld.cbl>SeqFileExample.cbl>PerformTimes.cbl>PerformExample.cbl>NestedIFExample.cbl>Message.cbl>IncrementExample.cbl>UserInput.cbl
+cobc -x MHelloWorld.cbl
